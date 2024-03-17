315a93431accf7f507cb49af0b43a38cfee6f6b3
Olaleye Blessing <Olayinkablexxy@gmail.com>
2024-03-17 23:42:32 +0100

    Backend: Enable client to sign up

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,14 @@
 import express, { Express } from 'express';
 import morgan from 'morgan';
 
+import authRouter from './routes/auth';
+
 const app: Express = express();
 
 if (process.env.NODE_ENV !== 'production') app.use(morgan('dev'));
+
+app.use(express.json());
+
+app.use('/api/v1/auth', authRouter);
 
 export default app;
---
+++ backend/src/models/user.ts
@@ -0,0 +1,63 @@
+import { Schema, model } from 'mongoose';
+import valdatior from 'validator';
+import { hash } from 'bcrypt';
+import { IUser } from './../interfaces/user';
+
+const userSchema = new Schema<IUser>({
+  name: {
+    type: String,
+    required: [true, 'Please provide your name'],
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide an email'],
+    validate: [valdatior.isEmail, 'Please provide a valid email'],
+    unique: true,
+  },
+  role: {
+    type: String,
+    required: [true, 'Please select the type of user you are.'],
+    enum: {
+      values: ['guest', 'manager'],
+      message: 'You can only be a guest or manager',
+    },
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minLength: [5, 'Password is too short'],
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: [
+      function validate(confirmPasword: string) {
+        // TODO: Properly type `this`
+        // @ts-expect-error This is not an error
+        return this.password === confirmPasword;
+      },
+      `Passwords are not the same`,
+    ],
+    select: false,
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+  updatedAt: Date,
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await hash(this.password, 12);
+  this.passwordConfirm = '';
+
+  next();
+});
+
+const User = model('User', userSchema);
+
+export default User;
