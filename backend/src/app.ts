0d0d38ac178b8fe6f525fa3a6da35eb8f280d387
Olaleye Blessing <Olayinkablexxy@gmail.com>
2024-03-17 21:36:09 +0100

    Backend: Log http request

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,8 @@
 import express, { Express } from 'express';
+import morgan from 'morgan';
 
 const app: Express = express();
+
+if (process.env.NODE_ENV !== 'production') app.use(morgan('dev'));
 
 export default app;
