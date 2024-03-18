c67590a6483ef9d3b9e0679b394c609a6b03d372
Olaleye Blessing <Olayinkablexxy@gmail.com>
2024-03-18 01:05:25 +0100

    Backend: Enable client to log in

--- backend/src/controllers/auth.ts
+++ backend/src/controllers/auth.ts
@@ -40,3 +40,52 @@ export const signup = async (req: Request, res: Response) => {
     });
   }
 };
+
+export const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  // TODO: Move this to a global error handler
+  if (!email && !password)
+    return res.status(400).json({
+      status: 'error',
+      message: 'Provide email and password',
+    });
+  if (!email)
+    return res.status(400).json({
+      status: 'error',
+      message: 'Provide email',
+    });
+  if (!password)
+    return res.status(400).json({
+      status: 'error',
+      message: 'Provide password',
+    });
+
+  try {
+    const user = await User.findOne({ email }).select('+password');
+
+    // Investigate this issue
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    if (!user || !(await user.correctPassword(password, user.password)))
+      return res.status(400).json({
+        status: 'error',
+        message: 'Incorrect email or password',
+      });
+
+    // @ts-expect-error Do not send password to the client;
+    user.password = undefined;
+
+    return res.status(201).json({
+      status: 'success',
+      data: {
+        user,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  }
+};
