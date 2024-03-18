ffd40e29a71cf1526b73ac208f783cd77283d8ab
Olaleye Blessing <Olayinkablexxy@gmail.com>
2024-03-18 12:17:17 +0100

    Backend: Send tokens after sign-up/log-in

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import express, { Express } from 'express';
 import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
 
 import authRouter from '@/routes/auth';
 import globalErrorHanlder from '@/controllers/error';
@@ -8,6 +9,7 @@ const app: Express = express();
 
 if (process.env.NODE_ENV !== 'production') app.use(morgan('dev'));
 
+app.use(cookieParser());
 app.use(express.json());
 
 app.use('/api/v1/auth', authRouter);
--- backend/src/interfaces/user.ts
+++ backend/src/interfaces/user.ts
@@ -1,3 +1,5 @@
+import { Types } from 'mongoose';
+
 export interface IUser {
   name: string;
   email: string;
@@ -12,4 +14,8 @@ export interface IUserWithNoCredential
   extends Omit<IUser, 'password' | 'passwordConfirm'> {
   password?: string;
   passwordConfirm?: string;
+}
+
+export interface IMongoUser extends IUser {
+  _id: Types.ObjectId;
 }
