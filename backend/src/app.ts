23dee1976b0f4f6094fb3075df88e07e36c03e64
Olaleye Blessing <Olayinkablexxy@gmail.com>
2024-03-20 21:38:03 +0100

    Both: Test production

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,7 +5,9 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 
 import authRouter from '@/routes/auth';
+import userRouter from '@/routes/user';
 import globalErrorHanlder from '@/controllers/error';
+import { protect } from './controllers/auth';
 
 const app: Express = express();
 
@@ -17,6 +19,8 @@ app.use(express.static(path.join(__dirname, '../../frontend/dist')));
 app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 
 app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', protect, userRouter);
+
 
 app.use(globalErrorHanlder);
 
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,64 +1,66 @@
 "use client";
 import { BACKEND_URL } from "@/constants/backend";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import Link from "next/link";
 import axios from "axios";
 import { useState } from "react";
+import { sleep } from "@/utils/sleep";
+import { handleIgbayesileAPIError } from "@/utils/handle-igbayesile-api-error";
 
 export default function Home() {
-  const mutation = useMutation({
-    mutationFn: async () => {
-      try {
-        console.log("logging in...");
-        let { data } = await axios.post(
-          `${BACKEND_URL}/api/v1/auth/login`,
-          {
-            email: "a@gmail.com",
-            password: "New Password",
-          },
-          {
-            withCredentials: true,
-          },
-        );
-        console.log(data);
-      } catch (error) {
-        throw error;
-      } finally {
-        console.log("login done");
-      }
-    },
-  });
-
   const [loadUsers, setLoadUsers] = useState(false);
 
-  const { isLoading, data, error } = useQuery({
+  const { data, error, refetch, isFetched, isFetching } = useQuery<
+    {
+      message: string;
+    },
+    Error
+  >({
     queryKey: ["users"],
     enabled: loadUsers,
     queryFn: async () => {
       try {
-        let { data } = await axios.get(`${BACKEND_URL}/api/v1/users`);
+        await sleep(3_000);
+        let { data } = await axios.get(`${BACKEND_URL}/api/v1/users`, {
+          withCredentials: true,
+        });
         return data;
       } catch (error) {
-        throw error;
+        throw new Error(handleIgbayesileAPIError(error));
       }
     },
+    retry: 0,
   });
 
-  console.log(isLoading, data, error);
+  console.log(data, error);
 
   return (
     <main>
-      <h1>Home Page</h1>
+      <h1 className=" text-center">Home Page</h1>
 
-      <button type="button" onClick={() => mutation.mutate()}>
-        Login In
-      </button>
+      <div className="my-4 flex flex-col items-center justify-center">
+        {isFetched ? (
+          <button type="button" onClick={() => refetch()}>
+            Refetch
+          </button>
+        ) : (
+          <button type="button" onClick={() => setLoadUsers(true)}>
+            Get Users
+          </button>
+        )}
+      </div>
 
-      <button type="button" onClick={() => setLoadUsers(true)}>
-        Get Users
-      </button>
+      <div className="flex items-center justify-center my-4">
+        {data ? (
+          <p className=" text-green-800">{data.message}</p>
+        ) : error ? (
+          <p className=" text-red-800">{error.message}</p>
+        ) : isFetching ? (
+          <p className="text-primary">Loading...</p>
+        ) : null}
+      </div>
 
-      <div>
+      <div className="flex items-center justify-center">
         <Link href={"/"}>Home</Link>
         <Link href={"/users"}>Users</Link>
       </div>
