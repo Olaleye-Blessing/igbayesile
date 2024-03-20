d8417cb1000d0b4deac3ed8336024d4e2c190130
Olaleye Blessing <Olayinkablexxy@gmail.com>
2024-03-20 21:22:43 +0100

    Both: Enable users to logout

--- frontend/components/navbar/index.tsx
+++ frontend/components/navbar/index.tsx
@@ -4,9 +4,32 @@ import useAuthStore from "@/stores/auth";
 import HomeLogo from "../home-logo";
 import Link from "next/link";
 import { Button, buttonVariants } from "../ui/button";
+import axios from "axios";
+import { API_BASE_URL } from "@/constants/backend";
+import toast from "react-hot-toast";
 
 const Navbar = () => {
   const user = useAuthStore((state) => state.user);
+  const storeLogout = useAuthStore((state) => state.logout);
+
+  const logout = async () => {
+    const toastId = "logging-out-user";
+
+    try {
+      storeLogout();
+
+      toast.loading("Leaving...", { id: toastId });
+      await axios.post(
+        `${API_BASE_URL}/auth/logout`,
+        {},
+        { withCredentials: true },
+      );
+
+      toast.success("See you soon", { id: toastId });
+    } catch (error) {
+      console.info(error);
+    }
+  };
 
   return (
     <nav className="border-b py-3" id="main-nav">
@@ -19,7 +42,12 @@ const Navbar = () => {
           {Boolean(user) ? (
             <div className="flex items-center justify-end">
               <p className="mr-2">avatar</p>
-              <Button variant="destructive" size="sm">
+              <Button
+                variant="destructive"
+                size="sm"
+                type="button"
+                onClick={logout}
+              >
                 Log Out
               </Button>
             </div>
