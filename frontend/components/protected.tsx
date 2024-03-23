e2299ddaeb24960d5731f37ee42af1aa43af8ceb
Olaleye Blessing <Olayinkablexxy@gmail.com>
2024-03-23 07:48:26 +0100

    Frontend: Create HOC to protect pages

---
+++ frontend/components/protected.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { redirect, usePathname, useSearchParams } from "next/navigation";
+import { PropsWithChildren, useEffect, useState } from "react";
+import Loading from "@/app/loading";
+import { IUser } from "@/interfaces/user";
+import useAuthStore from "@/stores/auth";
+import { useStore } from "@/stores/store";
+
+interface ProtectedProps extends PropsWithChildren {
+  role?: IUser["role"];
+}
+
+export default function Protected({ role, children }: ProtectedProps) {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const [authenticating, setAuthenticating] = useState(true);
+  const user = useStore(useAuthStore, (state) => state.user);
+
+  useEffect(
+    function checkIfUserIsAuthenticated() {
+      // zustand is yet to be initialized
+      if (user === undefined) return;
+
+      const redirectParams = searchParams.toString();
+      const redirectUrl =
+        `${pathname}?${encodeURIComponent(redirectParams)}`.replace(/\?$/, "");
+
+      // zustand is initialized but null
+      if (!user) return redirect(`/auth/login?redirect=${redirectUrl}`);
+
+      if (role && user.role !== role) redirect("/");
+
+      setAuthenticating(false);
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [user],
+  );
+
+  if (authenticating) return <Loading />;
+
+  if (!user) return null;
+
+  return <>{children}</>;
+}
