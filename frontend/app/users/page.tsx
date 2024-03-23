48684d36b78ec8bf5ecb27d974b4604563cd618e
Olaleye Blessing <Olayinkablexxy@gmail.com>
2024-03-23 11:26:37 +0100

    Frontend: Create hooks to make internal API Calls

--- frontend/app/users/page.tsx
+++ frontend/app/users/page.tsx
@@ -1,51 +1,21 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
 
-import { BACKEND_URL } from "@/constants/backend";
-import React, { useEffect, useState } from "react";
-
-const getUsers = async () => {
-  console.log("loading true");
-  let data: any = {
-    BACKEND_URL,
-    env: process.env.NODE_ENV,
-  };
-  try {
-    const req = await fetch(`${BACKEND_URL}/api/v1/users`, {
-      method: "get",
-      headers: {
-        "Content-type": "application/json",
-      },
-      credentials: "include",
-    });
-    const res = await req.json();
-    data = { ...data, ...res };
-    return { ...res, BACKEND_URL };
-  } catch (error) {
-    console.log("___ THERE IS AN ERROR ____");
-    data = { ...data, message: "Error" };
-  } finally {
-    console.log("Loading false");
-  }
-
-  return data;
-};
+import { useIGBQuery } from "@/hooks/use-igb-query";
 
 export default function Page() {
-  const [users, setUsers] = useState({});
-
-  useEffect(() => {
-    (async () => {
-      setUsers(await getUsers());
-    })();
-
-    return () => {};
-  }, []);
+  const { data: users, error } = useIGBQuery<{ message: string }>({
+    options: {
+      queryKey: ["users"],
+    },
+    url: `/users`,
+  });
 
   return (
     <div>
       <h1>Page</h1>
-      <p>{JSON.stringify(users)}</p>
+      {users && <p>{JSON.stringify(users)}</p>}
+      {error && <p>{JSON.stringify(error)}</p>}
     </div>
   );
 }
