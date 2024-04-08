978924b58db24bb7c813ab1d241e860c5c70bdc8
Olaleye Blessing <Olayinkablexxy@gmail.com>
2024-04-08 12:24:25 +0100

    Frontend: Provide easy access to switch theme

--- frontend/components/navbar/index.tsx
+++ frontend/components/navbar/index.tsx
@@ -2,13 +2,14 @@
 
 import useAuthStore from "@/stores/auth";
 import HomeLogo from "../home-logo";
-import Link from "next/link";
-import { Button, buttonVariants } from "../ui/button";
+import { Button } from "../ui/button";
 import axios from "axios";
 import { API_BASE_URL } from "@/constants/backend";
 import toast from "react-hot-toast";
 import Search from "./search";
 import LoggedInMenu from "./logged-in-menu";
+import UnAuthenticatedMenu from "./un-authenticated-menu";
+import { ThemeToggle } from "@/components/theme-toggle";
 
 const Navbar = () => {
   const user = useAuthStore((state) => state.user);
@@ -39,7 +40,8 @@ const Navbar = () => {
         <div>
           <HomeLogo />
         </div>
-        <Search className="hidden sm:block" />
+        <Search className="hidden sm:block sm:mx-auto" />
+        <ThemeToggle className="!p-0 ml-auto sm:ml-0" />
         <>
           {user ? (
             <div className="flex items-center justify-end">
@@ -55,20 +57,7 @@ const Navbar = () => {
               </Button>
             </div>
           ) : (
-            <div>
-              <Link
-                href="/auth/login"
-                className={buttonVariants({
-                  variant: "link",
-                  className: "mr-2 text-black hover:text-primary",
-                })}
-              >
-                Log In
-              </Link>
-              <Link href="/auth/signup" className={buttonVariants()}>
-                Create Account
-              </Link>
-            </div>
+            <UnAuthenticatedMenu />
           )}
         </>
       </div>
