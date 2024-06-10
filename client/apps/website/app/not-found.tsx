6c33d3e07927f8a63b94438653b83d26a5e7bb95
Olaleye Blessing <Olayinkablexxy@gmail.com>
2024-06-10 19:14:26 +0100

    Frontend: Add general metadata to main website

--- client/apps/website/app/not-found.tsx
+++ client/apps/website/app/not-found.tsx
@@ -1,5 +1,11 @@
 import NotFound from "@ui/components/not-found";
+import { Metadata } from "next";
 import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Igbayesile | Not Found",
+  description: "This page is not available on our server!",
+};
 
 export default function Page() {
   return (
