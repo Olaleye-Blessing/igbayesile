ca5b1a137f3e851d49bff8a1ec8767752e6f0e9e
Olaleye Blessing <Olayinkablexxy@gmail.com>
2024-06-20 13:18:17 +0100

    Frontend dashboard: Fix duplicate query key

--- client/apps/dashboard/app/[hotel]/rooms/_utils/query-factory-key.ts
+++ client/apps/dashboard/app/[hotel]/rooms/_utils/query-factory-key.ts
@@ -3,10 +3,10 @@ import { HotelQueryKeys } from "@dashboard/utils/hotel-factory-key";
 export const RoomQueryKeys = {
   base: (hotel: string) => [...HotelQueryKeys.hotel(hotel), "rooms"],
   rooms: (hotel: string, searchParams = "") => {
-    if (searchParams)
-      return [...RoomQueryKeys.base(hotel), "rooms", { searchParams }];
+    const key: any = [...RoomQueryKeys.base(hotel)];
+    if (searchParams) key.push({ searchParams });
 
-    return [...RoomQueryKeys.base(hotel), "rooms"];
+    return key;
   },
   room: ({ hotel, room }: { hotel: string; room: string }) => [
     ...RoomQueryKeys.rooms(hotel),
